54e60eeb86f3256e19f6b4b8512508b4c10b25ae
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-04-29 13:11:30 +0100

    Finishing the css of the audio player

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 let reproducerButton = document.querySelector('.reproducer');
 let libraryButton = document.querySelector('.library');
+let aboutReproducer = document.querySelector('.about-reproducer');
+let velocity = document.querySelector('.velocity');
+velocityOptions = document.querySelector('.velocity-options');
+let songList = document.querySelector('.song-list');
 libraryButton.addEventListener('click',()=>{
 	libraryButton.querySelector('span').style.color = '#7a7a7a7a';
 	libraryButton.style.borderBottom = '3px solid #7a6cc3';
 	reproducerButton.querySelector('span').style.color = '#76786d';
 	reproducerButton.style.borderBottom = 'none';
+	aboutReproducer.style.display = 'none';
+	songList.style.display = 'block';
 });
 
 reproducerButton.addEventListener('click',()=>{
@@ -12,6 +18,24 @@ reproducerButton.addEventListener('click',()=>{
 	reproducerButton.style.borderBottom = '3px solid #7a6cc3';
 	libraryButton.querySelector('span').style.color = '#76786d';
 	libraryButton.style.borderBottom = 'none';
+	aboutReproducer.style.display = 'block';
+	songList.style.display = 'none';
+});
+velocity.addEventListener('click',()=>{
+	velocityOptions.style.display = 'block';
+});
+
+document.addEventListener('click',(e)=>{
+	if(e.target.classList.contains('number') || e.target.classList.contains('velocity-options') || 
+		e.target.classList.contains('velocity') || e.target.classList.contains('velocity-span') ||
+		e.target.classList.contains('velocity-numbers')){
+		velocityOptions.style.display = 'block';
+	}else{
+		velocityOptions.style.display = 'none';
+	}
+});
+document.querySelector('.audio-play-button').addEventListener('click',()=>{
+	alert('oi');
 });
 
 /*FUNCOES*/
