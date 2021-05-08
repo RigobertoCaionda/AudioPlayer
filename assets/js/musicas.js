292e5e3cefb1a4eb30c416363ec488e0cdea1661
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-05-08 14:19:54 +0100

    Adding new songs and images

--- assets/js/musicas.js
+++ assets/js/musicas.js
@@ -1,9 +1,8 @@
 let musicas = [
-				{id: 0, author:'Sia Furler', title:'Broken Glass', img: 'assets/images/angeline.jpg',
+				{id: 0, author:'Sia Furler', title:'Broken Glass', img: 'assets/images/sia.jpg',
 				songUrl: 'assets/audio/BrokenGlass.mp3'},
-				{id: 1, author:'Rihanna', title:'Half Of Me', img: 'assets/images/angeline.jpg',
+				{id: 1, author:'Rihanna', title:'Half Of Me', img: 'assets/images/rihanna.jpg',
 				songUrl: 'assets/audio/Half Of Me-Rihanna.mp3'},
-				{id: 2, author:'James Blunt', title:"You're beautiful", img: 'assets/images/angeline.jpg',
-				songUrl: 'assets/audio/James_Blunt_-_Youre_Beautiful_Qoret.com.mp3'},
-				{id: 14, songUrl: 'assets/audio/BrokenGlass.mp3'}
+				{id: 2, author:'James Blunt', title:"You're beautiful", img: 'assets/images/james.jpg',
+				songUrl: 'assets/audio/James_Blunt_-_Youre_Beautiful_Qoret.com.mp3'}
 ];
